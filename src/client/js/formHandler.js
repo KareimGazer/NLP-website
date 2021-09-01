91de112f392c3ef42ff970fa2122c6820ddd4095
Kareim Tarek AbdelAzeem <kareimgazar1990@gmail.com>
2021-09-01 04:54:02 +0200

    meaningCloud Basic API

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,6 +1,9 @@
 function handleSubmit(event) {
   event.preventDefault();
 
+  // make a post req to send the url
+  // followed by a get req to get the data
+
   // check what text was put into the form field
   let formText = document.getElementById("name").value;
   Client.checkForName(formText);
--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,8 @@ const mockAPIResponse = require("./mockAPI.js");
 var bodyParser = require("body-parser");
 var cors = require("cors");
 
+const fetch = require("node-fetch");
+
 var json = {
   title: "test json response",
   message: "this is a message",
@@ -13,9 +15,9 @@ var json = {
 const dotenv = require("dotenv");
 dotenv.config();
 
-var textapi = new meaningCloud({
+var key = {
   application_key: process.env.API_KE,
-});
+};
 
 const app = express();
 app.use(cors());
@@ -37,10 +39,91 @@ app.get("/", function (req, res) {
 });
 
 app.get("/test", function (req, res) {
+  console.log("request", req);
   res.json(mockAPIResponse);
 });
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
   console.log("Example app listening on port 8081!");
+});
+
+// new code ---------------------------------------------------------------------
+
+var dataStore = {};
+// const info = {
+//   highlights: [],
+//   entities: [],
+//   concepts: [],
+// };
+
+const url = "https://api.meaningcloud.com/sentiment-2.1";
+
+const data = {
+  key: "fc19aa9b81a3379f18d00f553ccf96d8",
+  //txt: "I love you",
+  url: "https://blog.waymo.com/2021/08/addressing-transit-mobility-gaps-what.html",
+  lang: "en",
+};
+
+const callMeaningCloud = async (url, code, key) => {
+  const res = await fetch(url + code + key);
+
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+const getData = async (url = "", datain = {}) => {
+  console.log("Your data in", datain);
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // Body data type must match "Content-Type" header
+    body: JSON.stringify(datain),
+  });
+
+  try {
+    const newData = await response.json();
+    // const extractedData = newData.sentence_list;
+    // console.log("extracted data: ", extractedData);
+    // return extractedData;
+    console.log("new Data : ", newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+// this should be post
+app.get("/analyze", function (request, response) {
+  console.log("request to analyze", request);
+  getData(url, data).then((newData) => {
+    dataStore = newData;
+  });
+});
+
+app.get("/getInfo", function (request, response) {
+  // console.log("request to analyze", request);
+  console.log("processing data");
+  highlights = dataStore.sentence_list
+    .filter((sentence) => sentence.text.length > 100)
+    .map((sentence) => ({ text: sentence.text, score: sentence.score_tag }));
+  entities = dataStore.sentimented_entity_list.map((entity) => ({
+    form: entity.form,
+    type: entity.type,
+  }));
+  concepts = dataStore.sentimented_concept_list.map((concept) => ({
+    form: concept.form,
+    type: concept.type,
+  })); // use regex to get the type $ (end) at client
+  info = { highlights, entities, concepts };
+  response.send(info);
 });
